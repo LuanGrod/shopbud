20492dfd96ee542efb10a2c69cda471d8802c4c3
LuanGrod <luan14rodrigues17@gmail.com>
2026-05-21 21:59:41 -0300

    criacao das migrations

---
+++ database/migrations/2026_05_22_004423_create_shopping_items_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('shopping_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('session_id')->nullable()->index();
+            $table->string("sector_name", 256);
+            $table->string("product_name", 256);
+            $table->decimal("price");
+            $table->integer("quantity");
+            $table->boolean("extra");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('shopping_items');
+    }
+};
